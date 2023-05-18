4d31b90fe0e7cbac044f3fe61da2254a4c6d06e2
David Dyer <davidrossdyer@gmail.com>
2023-05-18 19:16:54 -0400

    update x2

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,10 +4,10 @@ import Footer from "../components/footer/footer.js";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex-row">
       <Nav />
-      <h1 className="text-4xl lg:text-6xl">Hello, Home!</h1>
+      <h1 className="text-4xl lg:text-6xl">MAIN CONTENT</h1>
       <Footer />
-    </>
+    </main>
   );
 }

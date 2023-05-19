b82ae832f912965006548a3f330a970d2964f99d
Julius M <juliusm9791@gmail.com>
2023-05-18 22:53:44 -0400

    test api

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,25 @@
 // HOME PAGE
+"use client";
+import React, { useState, useEffect } from "react";
 import Nav from "../components/nav/nav.js";
 import Footer from "../components/footer/footer.js";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    (async function () {
+      const response = await fetch("/api/hello");
+      const data = await response.json();
+      console.log(data);
+      setName(data);
+    })();
+  }, []);
+
   return (
     <main className="flex-row">
       <Nav />
       <h1 className="text-4xl lg:text-6xl">MAIN CONTENT</h1>
+      <p>{name.appName}</p>
       <Footer />
     </main>
   );

03dee771966ca5628b41be4ae09e69ad9535f90c
David Dyer <davidrossdyer@gmail.com>
2023-05-25 17:59:22 -0400

    nav added to register pages, home linked

--- src/app/components/nav/nav.js
+++ src/app/components/nav/nav.js
@@ -3,11 +3,14 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <nav className="flex-row">
-      <h1 className="px-2 py-4 bg-gray-800 text-gray-50"> HOME</h1>
+      <Link href="/">
+        {" "}
+        <h1 className="px-2 py-4 bg-gray-800 text-gray-50">HOME</h1>
+      </Link>
 
       <h1 className="px-2 py-4 bg-gray-800 text-gray-50">SEARCH</h1>
 
-      <Link href="/registration/signup">
+      <Link href="/registration/login">
         {" "}
         <h1 className="px-2 py-4 bg-gray-800 text-gray-50">PROFILE</h1>
       </Link>
--- src/app/registration/signup/page.js
+++ src/app/registration/signup/page.js
@@ -1,72 +1,84 @@
+import Link from "next/link";
+import Nav from "../../components/nav/nav.js";
+
 function Registration() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white p-8 rounded-lg">
-        <h1 className="text-2xl font-bold mb-4">Sign up</h1>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-gray-700 font-medium mb-2"
+    <div>
+      <Nav />
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md bg-white p-8 rounded-lg">
+          <h1 className="text-2xl font-bold mb-4">Sign up</h1>
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="border-gray-300 rounded-lg w-full py-2 px-3"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border-gray-300 rounded-lg w-full py-2 px-3"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="border-gray-300 rounded-lg w-full py-2 px-3"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                className="border-gray-300 rounded-lg w-full py-2 px-3"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600"
             >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="border-gray-300 rounded-lg w-full py-2 px-3"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-medium mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="border-gray-300 rounded-lg w-full py-2 px-3"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 font-medium mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="border-gray-300 rounded-lg w-full py-2 px-3"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="confirmPassword"
-              className="block text-gray-700 font-medium mb-2"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              className="border-gray-300 rounded-lg w-full py-2 px-3"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600"
-          >
-            Register
-          </button>
-        </form>
+              Register
+            </button>
+          </form>
+
+          <h5>
+            Already have an account? Click
+            <Link href="/registration/login"> here </Link>
+            to login.
+          </h5>
+        </div>
       </div>
     </div>
   );

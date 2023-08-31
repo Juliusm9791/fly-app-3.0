6cfc1060dc7026ad144ada312d56fcf1d6a1a9fd
Julius M <juliusm9791@gmail.com>
2023-08-30 23:09:23 -0400

    props for icons

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -18,7 +18,7 @@ export default function Nav() {
           className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <SearchIcon />
+          <SearchIcon height="150" width="150" color="#60a5fa" />
           <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
             Search
           </span>
@@ -70,7 +70,7 @@ export default function Nav() {
           className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <SettinsIcon />
+          <SettinsIcon color="red" />
 
           <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
             Settings

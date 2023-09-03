17f011dc9ce264b6fa3eabc1062a494325aa580b
David Dyer <davidrossdyer@gmail.com>
2023-09-02 22:39:00 -0400

    icons set up again

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -1,78 +1,72 @@
 import React from 'react';
 import Link from 'next/link';
-import { SearchIcon, SettinsIcon } from '@/common/icons';
+import {
+  DocumentsIcon,
+  SearchIcon,
+  SettingsIcon,
+  SettinsIcon,
+  TransfersIcon,
+  UserIcon,
+} from '@/common/icons';
 
 export default function Nav() {
   return (
-    <nav className="group flex-col items-center sm:bg-gray-900 position:absolute z-100 items-center w-20 h-screen hover:w-40">
-      <Link className="flex items-center w-full px-3 mt-6" href="/">
+    <nav className="pt-4 group flex-col items-center sm:bg-gray-900 absolute z-100 items-center w-20 h-screen hover:w-40">
+      <Link className="flex items-center w-full pl-5" href="/">
         <img
-          className="my-2 h-12 w-12 text-white hover:text-blue-400"
+          className="my-2 h-10 w-10 text-white hover:text-blue-400"
           src="/icons/fly_icon.svg"
           alt="flyapp_logo"
         />
       </Link>
 
-      <div className="pt-4 group flex flex-row sm:flex-col w-full mt-3 border-t border-gray-700">
+      <div className="pt-5 group flex flex-row sm:flex-col w-full mt-3 border-t border-gray-700">
         <Link
-          className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
+          className="group flex items-center w-full pl-3 pr-1 h-16 mt-1 ml-3 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <SearchIcon height="150" width="150" color="#60a5fa" />
-          <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
+          <SearchIcon color="#60a5fa" />
+          <span className="mb-8 -ml-4 text-sm font-medium hidden group-hover:block text-white">
             Search
           </span>
         </Link>
 
         <Link
-          className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
+          className="group flex items-center w-full h-18 pl-3 pr-2 ml-4 rounded text-white hover:text-blue-400"
           href="/auth/login"
         >
-          <img
-            className="flex items-center h-14 w-14 text-white"
-            src="/icons/user.svg"
-            alt="user"
-          />
-          <span className="mb-7 text-sm font-medium text-white hidden group-hover:block">
+          <UserIcon color="#60a5fa" />
+          <span className="mb-8 -ml-4 text-sm font-medium hidden group-hover:block text-white">
             Profile
           </span>
         </Link>
 
         <Link
-          className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
+          className="group flex items-center w-full h-12 px-3 mt-2 ml-4 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <img
-            className="flex items-center h-14 w-14 text-white hover:text-blue-400"
-            src="/icons/transfer.svg"
-            alt="transfer"
-          />
-          <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
-            Payments
+          <TransfersIcon color="#60a5fa" />
+          <span className="mb-10 -ml-3 text-sm font-medium hidden group-hover:block text-white">
+            Transfers
           </span>
         </Link>
 
         <Link
-          className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
+          className="group flex items-center w-full h-14 pl-3 pr-2 mt-2 ml-4 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <img
-            className="flex items-center h-14 w-14 text-white hover:text-blue-400"
-            src="/icons/documents.svg"
-            alt="documents"
-          />
-          <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
+          <DocumentsIcon color="#60a5fa" />
+          <span className="mb-11 -ml-4 text-sm font-medium hidden group-hover:block text-white">
             Documents
           </span>
         </Link>
 
         <Link
-          className="group flex items-center w-full h-12 px-3 mt-2 rounded text-white hover:text-blue-400"
+          className="group flex items-center w-full h-12 pl-3 pr-1 mt-2 ml-4 rounded text-white hover:text-blue-400"
           href="#"
         >
-          <SettinsIcon color="red" />
-
-          <span className="mb-7 text-sm font-medium hidden group-hover:block text-white">
+          <SettingsIcon color="#60a5fa" />
+          <span className="mb-11 -ml-6 text-sm font-medium hidden group-hover:block text-white">
             Settings
           </span>
         </Link>

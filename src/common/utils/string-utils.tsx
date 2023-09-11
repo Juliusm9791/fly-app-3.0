7d4e63a5da2833180f73112182dcd53727d9fc6c
Julius M <juliusm9791@gmail.com>
2023-09-11 09:03:17 -0400

    cleaned firebase error updates

--- src/common/utils/string-utils.tsx
+++ src/common/utils/string-utils.tsx
@@ -1,8 +1,10 @@
 export const firebaseErrorMsgStringClean = (error: string) => {
-  const cleanedErrorString = error.replace(/^auth\//, '').replace(/-/g, ' ');
-  const capFirstLetterErrorString =
-    cleanedErrorString.charAt(0).toUpperCase() +
-    cleanedErrorString.slice(1) +
-    '.';
-  return capFirstLetterErrorString;
+  if (error.includes('auth')) {
+    const cleanedErrorString = error.replace(/^auth\//, '').replace(/-/g, ' ');
+    const capFirstLetterErrorString =
+      cleanedErrorString.charAt(0).toUpperCase() +
+      cleanedErrorString.slice(1) +
+      '.';
+    return capFirstLetterErrorString;
+  } else return 'Password reset email sent Error. Try again later.';
 };

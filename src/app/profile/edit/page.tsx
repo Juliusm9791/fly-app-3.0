88af21da016d92c76904206bff29d84276dd2534
David Dyer <davidrossdyer@gmail.com>
2023-09-20 21:02:13 -0400

    more profile stuff do not delete me

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -4,11 +4,13 @@ import { redirect } from 'next/navigation';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '@/firebase-config';
 import InputForm from '@/common/input/input-form';
-import ButtonCommon from '@/common/input/button';
 import { UserProfile } from '@/common/variable-types';
 import { useSession } from 'next-auth/react';
+import ButtonSubmit from '@/common/input/button-submit';
+import { useRouter } from 'next/navigation';
 
 export default function ProfileEdit() {
+  const router = useRouter();
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
@@ -43,16 +45,18 @@ export default function ProfileEdit() {
         email: userProfile.email,
         firstName: userProfile.firstName,
         middleName: '',
-        lastName: userProfile.firstName,
+        lastName: userProfile.lastName,
         avatar: '',
-        address: '',
-        phone: '',
+        address: userProfile.address,
+        phone: userProfile.phone,
         emailVerified: userProfile.emailVerified,
       });
       console.log(res);
       console.log('Data added successful.');
     } catch (error) {
       console.error(error);
+    } finally {
+      router.push('/profile/dash');
     }
   };
 
@@ -60,47 +64,49 @@ export default function ProfileEdit() {
     <>
       <div>Edit Profile</div>
 
-      <div className="p-10 flex flex-col bg-gray-100 rounded mt-20">
-        <InputForm
-          label="Email"
-          value={userProfile.email}
-          name="Email"
-          onChange={handleProfileInput}
-        ></InputForm>
+      <form
+        className="flex flex-col items-center"
+        onSubmit={handleSubmitProfile}
+      >
+        <div className="p-6 flex flex-col bg-gray-100 rounded mt-20">
+          <InputForm
+            label="Email"
+            value={userProfile.email}
+            name="Email"
+            onChange={handleProfileInput}
+          ></InputForm>
 
-        <InputForm
-          label="First Name"
-          value={userProfile.firstName}
-          name="firstName"
-          onChange={handleProfileInput}
-        ></InputForm>
+          <InputForm
+            label="First Name"
+            value={userProfile.firstName}
+            name="firstName"
+            onChange={handleProfileInput}
+          ></InputForm>
 
-        <InputForm
-          label="Last Name"
-          value={userProfile.lastName}
-          name="lastName"
-          onChange={handleProfileInput}
-        ></InputForm>
+          <InputForm
+            label="Last Name"
+            value={userProfile.lastName}
+            name="lastName"
+            onChange={handleProfileInput}
+          ></InputForm>
 
-        <InputForm
-          label="Address"
-          value={userProfile.address}
-          name="address"
-          onChange={handleProfileInput}
-        ></InputForm>
+          <InputForm
+            label="Address"
+            value={userProfile.address}
+            name="address"
+            onChange={handleProfileInput}
+          ></InputForm>
 
-        <InputForm
-          label="Phone"
-          value={userProfile.phone}
-          name="phone"
-          onChange={handleProfileInput}
-        ></InputForm>
+          <InputForm
+            label="Phone"
+            value={userProfile.phone}
+            name="phone"
+            onChange={handleProfileInput}
+          ></InputForm>
 
-        <ButtonCommon
-          label="Save"
-          onButtonClick={handleSubmitProfile}
-        ></ButtonCommon>
-      </div>
+          <ButtonSubmit label="Save"></ButtonSubmit>
+        </div>
+      </form>
     </>
   );
 }
